51b23f321d06c050e84a6630cbf948fb63fdb7f1
Myron R Schippers Jr <dubtwee@gmail.com>
2023-06-07 16:26:36 -0400

    making the default selected color black

--- src/app/color-picker/color-picker.component.ts
+++ src/app/color-picker/color-picker.component.ts
@@ -1,17 +1,16 @@
-import {Component, Input, OnInit} from '@angular/core';
+import { Component, Input, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-color-picker',
-  templateUrl: './color-picker.component.html',
-  styleUrls: ['./color-picker.component.scss']
+  selector: "app-color-picker",
+  templateUrl: "./color-picker.component.html",
+  styleUrls: ["./color-picker.component.scss"],
 })
 export class ColorPickerComponent implements OnInit {
   @Input() colorOptions: string[];
   @Input() initialColor: string;
-  selectedColor : string;
+  selectedColor: string;
 
   ngOnInit(): void {
+    this.selectedColor = "black";
   }
-
-
 }
